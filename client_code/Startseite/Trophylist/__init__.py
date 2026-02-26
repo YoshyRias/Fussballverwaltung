3fe96629928b151b62666587d0a43d2651811f83
jonas.marte@student.htl-rankweil.at <jonas.marte@student.htl-rankweil.at>
2026-02-26 11:27:48 +0000

    Created Startseite.Trophylist.RowTemplate1, Startseite, Startseite.Trophylist and Startseite.RowTemplate; Edited settings and Backend; Deleted Frontend.RowTemplate1, Frontend.RowTemplate2 and Frontend

--- client_code/Startseite/Trophylist/__init__.py
+++ client_code/Startseite/Trophylist/__init__.py
@@ -1,13 +1,22 @@
-from ._anvil_designer import RowTemplate2Template
+from ._anvil_designer import TrophylistTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class RowTemplate2(RowTemplate2Template):
+
+class Trophylist(TrophylistTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    query = '''
+    SELECT t.name, ft.jahr, m.kategorie 
+    FROM Fussballverein_Trophaeen ft
+    JOIN Trophaeen t ON t.TrID = ft.TrID
+    JOIN Mannschaft m ON m.FID = ft.FID
+    WHERE 
+    
+    '''
